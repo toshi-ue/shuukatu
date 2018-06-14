e93bd89a25a3c1fa26e7287c3e5aaedf9e534875
mac <toshi.import@gmail.com>
2018-06-14 20:12:59 +0900

    add function pagination in genres/show.

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -1,7 +1,8 @@
 class Public::GenresController < ApplicationController
 
   def show
-    @items = Item.where(genre_id: params[:id])
+    @items = Item.where(genre_id: params[:id]).page(params[:page]).per(20)
+    # binding.pry
   end
 
 end
