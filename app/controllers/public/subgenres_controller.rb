b1491f4c6a53c6f21081b046ec03aa6cefecfce7
mac <toshi.import@gmail.com>
2018-06-14 15:42:56 +0900

    run 'rails g controller public::subgenres'.

---
+++ app/controllers/public/subgenres_controller.rb
@@ -0,0 +1,2 @@
+class Public::SubgenresController < ApplicationController
+end
---
+++ test/controllers/public/subgenres_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Public::SubgenresControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
