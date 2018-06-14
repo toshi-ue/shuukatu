9f463e87c2889593f06773567da7594bd1112fea
mac <toshi.import@gmail.com>
2018-06-15 01:14:56 +0900

    add function judgement of path.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   # 円表示用
-  def dispMoney(money)
+  def dispYen(money)
     price = money.to_s
     if price.end_with?('.0')
       price = price.to_i
@@ -10,4 +10,21 @@ module ApplicationHelper
       "#{number_to_currency(item.price, :format => "%u%n", :precision => 1, :unit =>"¥")}"
     end
   end
+
+  # https://qiita.com/Vit-Symty/items/55497adb17d476d36d69
+  # スコープ判定
+  def scope_name
+    "#{(controller.class.parent.name).downcase}"
+  end
+
+  # コントローラー判定
+  # def controller_name
+  #   "#{controller.controller_name}"
+  # end
+
+  # アクション判定
+  # def action_name
+  #   "#{action_name}"
+  # end
+
 end
