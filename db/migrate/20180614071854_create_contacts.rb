3c0b2d0b4e7e31befc5153229a08937665685920
mac <toshi.import@gmail.com>
2018-06-14 16:21:18 +0900

    run 'rake db:migrate'.

--- db/migrate/20180614071854_create_contacts.rb
+++ db/migrate/20180614071854_create_contacts.rb
@@ -1,8 +1,11 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-
-      t.timestamps null: false
+      t.integer  "quantity"
+      t.integer  "item_id"
+      t.integer  "order_id"
+      t.integer  "user_id"
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180614071443) do
+ActiveRecord::Schema.define(version: 20180614071854) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id",       limit: 4
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20180614071443) do
   end
 
   create_table "cartitems", force: :cascade do |t|
+    t.integer  "quantity",   limit: 4
+    t.integer  "item_id",    limit: 4
+    t.integer  "order_id",   limit: 4
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", force: :cascade do |t|
     t.integer  "quantity",   limit: 4
     t.integer  "item_id",    limit: 4
     t.integer  "order_id",   limit: 4
