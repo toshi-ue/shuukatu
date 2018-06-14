f66bec69bbce8fbb930b2136e7a97a63b638361b
mac <toshi.import@gmail.com>
2018-06-14 18:05:59 +0900

    change layout tops/index .

--- app/controllers/public/tops_controller.rb
+++ app/controllers/public/tops_controller.rb
@@ -1,6 +1,9 @@
 class Public::TopsController < ApplicationController
   def index
-    @newItems = Item.order(updated_at: :desc).limit(4)
+    @newItems = Item.order(updated_at: :desc).limit(6)
+    @genre1Items = Item.where(genre_id: 1).order(updated_at: :desc).limit(6)
+    @genre2Items = Item.where(genre_id: 2).order(updated_at: :desc).limit(6)
+    @genre3Items = Item.where(genre_id: 3).order(updated_at: :desc).limit(6)
   end
 
   def company
