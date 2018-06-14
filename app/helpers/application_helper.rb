45ee3981ada259afbdbab3cd92e640e73a2ccc42
mac <toshi.import@gmail.com>
2018-06-14 19:27:26 +0900

    add helper function of displaying yen.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # 円表示用
+  def dispMoney(money)
+    price = money.to_s
+    if price.end_with?('.0')
+      price = price.to_i
+      "#{number_to_currency(price, :format => "%u%n", :precision => 0, :unit =>"¥")}"
+    else
+      "#{number_to_currency(item.price, :format => "%u%n", :precision => 1, :unit =>"¥")}"
+    end
+  end
 end
