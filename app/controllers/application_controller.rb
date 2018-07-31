dcf911683772213d25bac67339476f7b626f0cc0
mac <toshi.import@gmail.com>
2018-08-01 01:27:36 +0900

    確認用sessionの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :request_path
-  before_action :controller_session
+  before_action :set_controller_session
   before_action :count_cartitems, if: :user_signed_in?
 
   # bootstrapフラッシュメッセージ用
   add_flash_types :success, :info, :warning, :danger
 
-# https://qiita.com/toduq/items/a9fa48926b060b2d8a5b
   def request_path
-      @path = controller_path + '#' + action_name
-      def @path.is(*str)
-          str.map{|s| self.include?(s)}.include?(true)
-      end
+    @path = controller_path + '#' + action_name
   end
 
-  def controller_session
-
-    session[:fifth_controller] = session[:fourth_controller]
-    session[:fourth_controller] = session[:third_controller]
-    session[:third_controller] = session[:second_controller]
-    session[:second_controller] = session[:first_controller]
-    session[:first_controller] = @path
+  def set_controller_session
+    if session[:first_controller].blank?
+      session[:first_controller] = @path
+    else
+      session[:second_controller] = session[:first_controller]
+      session[:first_controller] = @path
+    end
   end
 
   def count_cartitems
