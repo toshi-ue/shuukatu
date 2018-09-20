c0b25e17b9016876b8e8d589e67c8d568e9d5f3d
mac <toshi.import@gmail.com>
2018-09-21 01:39:16 +0900

    re-edit migration

--- db/migrate/20180920162124_add_soft_destroyed_at_to_your_model.rb
+++
@@ -1,6 +0,0 @@
-class AddSoftDestroyedAtToYourModel < ActiveRecord::Migration
-  def change
-    add_column :your_models, :soft_destroyed_at, :datetime
-    add_index :your_models, :soft_destroyed_at
-  end
-end
