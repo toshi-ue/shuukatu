24fe8d38e7e2339dbf182a742f72f43f270cef1a
toshi-ue <toshihiro.u.0207@gmail.com>
2019-02-15 18:58:15 +0900

    update: userモデルスペックファイルの修正
    不要な部分の削除、パスワード暗号化のテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,75 +11,62 @@ RSpec.describe User, type: :model do
     end
 
     context "valid" do
-      context "when メールアドレスでログイン" do
-        it "email, password, password_confirmation に全て値が入力されている" do
-          expect(@user.valid?).to eq(true)
-        end
+      context "when sign-up" do
+        context "by email" do
+          it "email, password, password_confirmation に全て値が入力されている" do
+            expect(@user.valid?).to eq(true)
+          end
 
-        it "password が8文字以上" do
-          expect(@user.password.length).to be >= 8
-        end
+          it "password が8文字以上" do
+            expect(@user.password.length).to be >= 8
+          end
 
-        it "password が英数それぞれ1文字以上を含む" do
-          expect(@user.password).to match(/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,126}+\z/i)
-        end
+          it "password が英数それぞれ1文字以上を含む" do
+            expect(@user.password).to match(/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,126}+\z/i)
+          end
 
-        it "password, password_confirmation が同値" do
-          @user.valid?
-          expect(@user.password).to eq @user.password_confirmation
-        end
-      end
+          it "password, password_confirmation が同値" do
+            @user.valid?
+            expect(@user.password).to eq @user.password_confirmation
+          end
 
-      context "when SNSログインでログイン" do
-        # TODOS SNSログインのテストを実装
-        it "facebookログイン" do
-          pending "facebookログインのテストわからず"
-          expect(bb).to eq "aaa"
-        end
+          it "emailがuniqueであること" do
+            user2 = FactoryBot.create(:user)
+            user2.valid?
+            expect(user2.email).to_not eq @user.email
+          end
 
-        it "googleログイン" do
-          pending "googleログインのテストわからず"
-          expect(bb).to eq "aaa"
+          it "password_digestで暗号化" do
+            user =  FactoryBot.create(:user)
+            expect(user.encrypted_password).to_not eq "password12"
+          end
         end
       end
     end
 
     context "invalid" do
-      context "when メールアドレスでログイン" do
-        it "email が未入力" do
-          @user.email = nil
-          expect(@user.valid?).to eq(false)
-        end
+      context "when sign up" do
+        context "by email" do
+          it "email が未入力" do
+            @user.email = nil
+            expect(@user.valid?).to eq(false)
+          end
 
-        it "password が8文字以下" do
-          @user.password = "a"
-          expect(@user.valid?).to eq(false)
-        end
+          it "password が8文字以下" do
+            @user.password = "a"
+            expect(@user.valid?).to eq(false)
+          end
 
-        it "password, password_confirmation が異なる値" do
-          @user.password = "a"
-          @user.valid?
-          expect(@user.password).to_not eq @user.password_confirmation
-        end
+          it "password, password_confirmation が異なる値" do
+            @user.password = "a"
+            @user.valid?
+            expect(@user.password).to_not eq @user.password_confirmation
+          end
 
-        it "重複メールアドレスが存在" do
-          user2 = FactoryBot.create(:user)
-          @user.valid?
-          # expect(user.errors[:email]).to include("has already been taken")
-          expect(@user.valid?).to eq(false)
-        end
-      end
-
-      context "when SNSでログイン" do
-        # TODOS SNSログインのテストを実装
-        it "facebookログイン" do
-          pending "facebookログインのテストわからず"
-          expect(bb).to eq "aaa"
-        end
-
-        it "googleログイン" do
-          pending "googleログインのテストわからず"
-          expect(bb).to eq "aaa"
+          it "emailの重複" do
+            user2 = FactoryBot.create(:user, email: @user.email)
+            expect(@user.valid?).to eq(false)
+          end
         end
       end
     end
