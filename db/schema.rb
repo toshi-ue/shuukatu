bcbcf2c7e4706ca726ea67af9408fa641240af44
toshi-ue <toshihiro.u.0207@gmail.com>
2019-07-07 09:31:11 +0900

    🎨 add: itemモデルに各itemのlike数を合計するカラムを追加(like_count)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190506083539) do
+ActiveRecord::Schema.define(version: 20190707002309) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id",       limit: 4
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 20190506083539) do
     t.string   "image3",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "likes_count", limit: 4
   end
 
   create_table "likes", force: :cascade do |t|
