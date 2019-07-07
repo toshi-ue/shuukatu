098342a285e277bbc7439c1806d1fa4c9adab125
toshi-ue <toshihiro.u.0207@gmail.com>
2019-07-07 09:46:06 +0900

    🐛 update: カラム定義の修正(likes_count)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190707002309) do
+ActiveRecord::Schema.define(version: 20190707004318) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id",       limit: 4
@@ -112,7 +112,7 @@ ActiveRecord::Schema.define(version: 20190707002309) do
     t.string   "image3",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "likes_count", limit: 4
+    t.integer  "likes_count", limit: 4,   default: 0, null: false
   end
 
   create_table "likes", force: :cascade do |t|
