6b508338fcaaa176880f01abe3e7e0480e9b9be3
toshi-ue <toshihiro.u.0207@gmail.com>
2019-09-26 20:03:09 +0900

    🐛 update: テストデータの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
-  factory :user do
-      sequence(:email) {|n| "tester#{n}@example.com"}
-      sequence(:password) {"password12"}
-      sequence(:password_confirmation) {"password12"}
-      sequence(:provider)
-      sequence(:uid)
+  factory :user, aliases: [:owner] do
+    sequence(:email) {|n| "tester#{n}@example.com"}
+    sequence(:password) {"password12"}
+    sequence(:password_confirmation) {"password12"}
+    sequence(:provider)
+    sequence(:uid)
   end
 end
